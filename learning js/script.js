0d484584d7331a5551dae30d771484ef742288b4
NajeebSultan <najeebsultan070@gmail.com>
2025-07-30 21:34:36 +0500

    Add for loop examples for table of 5 and nested loops

--- learning js/script.js
+++ learning js/script.js
@@ -75,3 +75,16 @@ for(let i = 1; i <= 15; i += 2) {
 for(let i = 0 ; i<=10 ;i+= 2) {
     console.log(i);
 }
+
+//Table of 5 
+for(let i=5 ; i<=50; i+=5) {
+    console.log(i);
+}
+
+//nested for loop
+for(let i = 1; i <= 3; i++) {
+  console.log(`outer loop ${i}`);
+    for(let j = 1; j <= 3; j++) {
+        console.log(j);
+    }
+}
