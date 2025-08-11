a04384f287a7a6a9e88e0fe771cc3f67d1b6996e
NajeebSultan <najeebsultan070@gmail.com>
2025-08-12 00:57:00 +0500

    Make a div with h1 and paragraph text

--- learning js/App.js
+++ learning js/App.js
@@ -10,3 +10,13 @@ document.querySelector("body").append(h3);
 
 h3.classList.add("blue");
 
+
+let div = document.createElement("div");
+let h1 = document.createElement("h1");
+let para2 = document.createElement("p");
+
+h1.innerText = "I m in a div";
+para2.innerText = "Me TOO";
+
+div.append(h1);
+div.append(para2);
