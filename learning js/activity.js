3a617fb3df14a49728d48730716163a869b9260f
NajeebSultan <najeebsultan070@gmail.com>
2025-08-14 23:51:39 +0500

    Random color generator is done

--- learning js/activity.js
+++ learning js/activity.js
@@ -1,23 +1,21 @@
 let btn = document.querySelector("button");
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function () {
   let h3 = document.querySelector("h3");
-  let randomColor = getRandomColor;
-  h3.innerText = randomColor ; 
+  let randomColor = getRandomColor(); // <-- call the function
+  h3.innerText = randomColor;
 
   let div = document.querySelector("div");
   div.style.backgroundColor = randomColor;
 
   console.log("color updated");
-  
 });
 
 function getRandomColor() {
-  let red = Math.floor(Math.random() *255) ;
-  let green  = Math.floor(Math.random() *255) ;
-  let blue  = Math.floor(Math.random() *255) ;
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
 
-  let color = `rgb(${red} , ${green} ,${blue})`;
-return color;
-
+  let color = `rgb(${red}, ${green}, ${blue})`;
+  return color;
 }
