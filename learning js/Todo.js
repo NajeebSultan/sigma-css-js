ce9aae98cc4e7e0b06afc534af29a31da0944ba2
NajeebSultan <najeebsultan070@gmail.com>
2025-08-18 14:40:08 +0500

    Add delete button functionality to todo list items

--- learning js/Todo.js
+++ learning js/Todo.js
@@ -5,7 +5,21 @@ let inp = document.querySelector("input");
 btn.addEventListener("click", function () {
   let item = document.createElement("li");
   item.innerText = inp.value; 
-  ul.appendChild(item); // Add the new item to the list
-  inp.value = ""; // Clear the input field after adding the item
+
+  let delBtn = document.createElement("button");
+delBtn.innerText= "delete";
+delBtn.classList.add("delete");
+
+item.appendChild(delBtn); // Add the delete button to the item
+ul.appendChild(item); // Add the new item to the list
+inp.value = ""; // Clear the input field after adding the item
 
 });
+
+let delBtns = document.querySelectorAll(".delete");
+for(delBtns of delBtns) {
+  delBtns.addEventListener("click", function () {
+    let par = this.parentElement; // Get the parent <li> element
+    par.remove(); // Remove the <li> from the list
+  });
+} 
