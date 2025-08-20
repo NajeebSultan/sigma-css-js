7500853d096ddd0f8bf10274588718c3484e2338
NajeebSultan <najeebsultan070@gmail.com>
2025-08-20 15:33:04 +0500

    Refactor Api.js to restore button click event handler and image fetching functionality

--- learning js/Api.js
+++ learning js/Api.js
@@ -2,26 +2,20 @@ let btn = document.querySelector("button");
 let url2 = "https://dog.ceo/api/breeds/image/random";
 
 
+btn.addEventListener("click", async () => {
+  // let fact = await getFacts();
+  // console.log(fact);
+  // let p = document.querySelector("#result");
+  // p.innerText = fact;
+});
 
 
-
-
-// btn.addEventListener("click", async () => {
-//   let fact = await getFacts();
-//   console.log(fact);
-//   let p = document.querySelector("#result");
-//   p.innerText = fact;
-// });
-
-
-// let url = "https://catfact.ninja/fact";
-
-// async function getFacts() {
-//   try{
-//     let res = await axios.get(url);
-//     return res.data.fact;
-//   } catch(e) {
-//     console.log("ERROR- ", e);
-//     return "No fact found";
-//   }
-// }
+async function getImage() {
+  try{
+    let res = await axios.get(url2);
+    return res.data.message;
+  } catch(e) {
+    console.log("ERROR- ", e);
+    return "No image found";
+  }
+}
