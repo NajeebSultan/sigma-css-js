9678a4c2f188e6af59f7eb5f9e10419a254a42a3
NajeebSultan <najeebsultan070@gmail.com>
2025-08-20 15:19:14 +0500

    Fix button event handler to target result paragraph and improve error logging

--- learning js/Api.js
+++ learning js/Api.js
@@ -3,18 +3,19 @@ let btn = document.querySelector("button");
 btn.addEventListener("click", async () => {
   let fact = await getFacts();
   console.log(fact);
-  let p = document.querySelector("p");
+  let p = document.querySelector("#result");
   p.innerText = fact;
 });
 
 
 let url = "https://catfact.ninja/fact";
+
 async function getFacts() {
   try{
     let res = await axios.get(url);
     return res.data.fact;
-  } catch(err) {
-    console.error("ERROR- ", err);
+  } catch(e) {
+    console.log("ERROR- ", e);
     return "No fact found";
   }
 }
