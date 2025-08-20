8dbce10a704960a67983d531e3c33f793f8433da
NajeebSultan <najeebsultan070@gmail.com>
2025-08-20 15:31:09 +0500

    Update Api.html to change heading and button text for new functionality

--- learning js/Api.js
+++ learning js/Api.js
@@ -1,21 +1,27 @@
 let btn = document.querySelector("button");
-
-btn.addEventListener("click", async () => {
-  let fact = await getFacts();
-  console.log(fact);
-  let p = document.querySelector("#result");
-  p.innerText = fact;
-});
+let url2 = "https://dog.ceo/api/breeds/image/random";
 
 
-let url = "https://catfact.ninja/fact";
 
-async function getFacts() {
-  try{
-    let res = await axios.get(url);
-    return res.data.fact;
-  } catch(e) {
-    console.log("ERROR- ", e);
-    return "No fact found";
-  }
-}
+
+
+
+// btn.addEventListener("click", async () => {
+//   let fact = await getFacts();
+//   console.log(fact);
+//   let p = document.querySelector("#result");
+//   p.innerText = fact;
+// });
+
+
+// let url = "https://catfact.ninja/fact";
+
+// async function getFacts() {
+//   try{
+//     let res = await axios.get(url);
+//     return res.data.fact;
+//   } catch(e) {
+//     console.log("ERROR- ", e);
+//     return "No fact found";
+//   }
+// }
